89388eda0da6cf57833e75e2739b324ea1f1f77b
leanhtuanwru <tuanla520@wru.vn>
2018-03-29 21:56:31 +0700

    Change Pass MySQL

--- includes/config.php
+++ includes/config.php
@@ -1,14 +1,10 @@
 <?php
 // Thông tin Database
-<<<<<<< HEAD
+
 $conn = mysqli_connect('localhost', 'root', '12345678', 'webshop') or die ('Không thể kết nối tới database');
 mysqli_set_charset($conn,"utf8");
-=======
 
 
-$conn = mysqli_connect('localhost', 'root', 'mysql', 'webshop') or die ('Không thể kết nối tới database');
 
-mysqli_set_charset($conn,"utf8"); 
 
->>>>>>> 123c03bc908c056fefc3dfd10811d52115a7419d
 ?>
